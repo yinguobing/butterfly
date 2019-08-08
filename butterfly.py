ba40f55c104c223509acc66eb6b321989a7aed91
yinguobing <yinguobing@gmail.com>
2019-08-08 11:24:45 +0800

    initial commit

---
+++ butterfly.py
@@ -0,0 +1,45 @@
+"""A lightwight module to load TensorFlow frozen model"""
+import tensorflow as tf
+
+
+class Butterfly(object):
+
+    def __init__(self, model_filename, input_node_name, output_node_names):
+        """
+        Args:
+            model_filename: the frozen model's filename.
+            input_node_name: the name of the input node.
+            output_node_names: the name of the output nodes as a list.
+        """
+
+        # load the frozen model file and parse it to retrieve the GraphDef.
+        with tf.gfile.GFile(model_filename, "rb") as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+
+        # Import the graph_def into a new Graph and create a session.
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            tf.import_graph_def(graph_def, name='butterfly')
+            self.sess = tf.Session(graph=self.graph)
+
+        self.input_node_name = input_node_name
+        self.output_node_names = output_node_names
+
+    def list_ops(self):
+        """Output the op names in the graph."""
+        names = [op.name for op in self.graph.get_operations()]
+        return names
+
+    def run(self, inputs):
+        """Run inference and return the results.
+        Args:
+            inputs: inputs as a list.
+
+        Returns:
+            inference results.
+        """
+        input_node = self.graph.get_tensor_by_name(self.input_node_name + ":0")
+        output_node = self.graph.get_tensor_by_name(
+            self.output_node_names + ":0")
+        return self.sess.run(output_node, feed_dict={input_node: inputs})
---
+++ demo.py
@@ -0,0 +1,56 @@
+import argparse
+import cv2
+from butterfly import Butterfly
+
+# Parse the arguments user entered.
+parser = argparse.ArgumentParser()
+parser.register("type", "bool", lambda v: v.lower() == "true")
+parser.add_argument("--model", default="frozen_model.pb",
+                    type=str, help="Frozen model file to import")
+parser.add_argument("--list_ops", default="false",
+                    type="bool", help="List all the ops in the model graph")
+parser.add_argument("--image", default=None,
+                    type=str, help="Input image file")
+parser.add_argument("--video", default=None,
+                    type=str, help="Input video file")
+parser.add_argument("--cam", default=None,
+                    type=int, help="Camera index number")
+parser.add_argument("--input_node", default="",
+                    type=str, help="The name of the input node")
+parser.add_argument("--output_node", default="",
+                    type=str, help="The name of the output node")
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    # Construct a Butterfly.
+    fly = Butterfly(args.model, args.input_node, args.output_node)
+
+    # Output all the ops name in the graph.
+    if args.list_ops:
+        print(fly.list_ops())
+
+    # Process an image.
+    if args.image:
+        image = cv2.imread(args.image)
+        print(fly.run([image]))
+
+    # Process video/cam.
+    if args.cam is not None:
+        video_source = args.cam
+    elif args.video:
+        video_source = args.video
+    else:
+        video_source = None
+
+    if video_source is not None:
+        cap = cv2.VideoCapture(video_source)
+        while True:
+            _, frame = cap.read()
+
+            result = fly.run([frame])
+            print(result)
+
+            cv2.imshow("Preview", frame)
+            if cv2.waitKey(27) == 27:
+                break
